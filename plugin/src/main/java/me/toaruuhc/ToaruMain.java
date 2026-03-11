632d8dfd330e5ace41aaf9528a2312e81aac468a
Medsir <mehdibenmer@gmail.com>
2026-03-11 22:49:33 +0100

    Structure project

---
+++ plugin/src/main/java/me/toaruuhc/ToaruMain.java
@@ -0,0 +1,17 @@
+package me.toaruuhc;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ToaruMain extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Ce message s'affichera dans la console de ton serveur Minecraft
+        getLogger().info("Le systeme de la Cite Scolaire est en ligne !");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("Arret du systeme Toaru.");
+    }
+}
